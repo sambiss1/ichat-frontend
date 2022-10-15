ee5d40e531e252d4652092297ef1ac5e6434a42c
Sam <sambisselele@gmail.com>
2022-10-15 17:37:46 +0100

    Feat(Fetching conversations participants)

--- src/components/RecentMessages/index.js
+++ src/components/RecentMessages/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import "./recentsMessages.css"
+import { React, useState, useEffect } from 'react'
+
+import axios from 'axios';
+import "./recentsMessages.css";
 
 
-const RecentMessageCard = () => {
+const RecentMessageCard = ({ props }) => {
     return (
         <div className="recent__message--card">
             <div className="recent__message--user__picture">
@@ -13,7 +15,7 @@ const RecentMessageCard = () => {
                 />
             </div>
             <div className="recent__message--user__message">
-                <h3>Raghav</h3>
+                <h3>{props.userName}</h3>
                 <p>Dinner ? </p>
             </div>
 
@@ -22,16 +24,51 @@ const RecentMessageCard = () => {
 }
 
 const RecentsMessages = () => {
+    const [recentMessages, setRecentMessages] = useState([]);
+    const [participants, setParticipants] = useState([]);
+
+    useEffect(() => {
+        const fetchConversation = async () => {
+            await axios.get("http://localhost:8000/api/conversations/")
+                .then((response) => {
+                    setRecentMessages(response.data.conversations)
+                    console.log(recentMessages);
+                    // recentMessages.map((conversation) => setParticipants(conversation.participants))
+                    // console.log(participants)
+                })
+                .catch(error => alert(error));
+        }
+
+        fetchConversation();
+    }, [])
+
+    console.log(participants)
 
     return (
         <div className="recent__message--main__container">
             <h3>Recent</h3>
             <div className="recent__message--list">
-                <RecentMessageCard />
-                <RecentMessageCard />
-                <RecentMessageCard />
-                <RecentMessageCard />
-                <RecentMessageCard />
+
+                {
+                    recentMessages.length <= 0 ?
+                        (<h3>Loading...</h3>) :
+                        (recentMessages.map((conversation) =>
+                            conversation.participants[1] &&
+
+                            <RecentMessageCard
+                                props={conversation.participants[1]}
+                                key={conversation.participants._id}
+
+                            />
+
+                        )
+                        )
+
+                    
+
+                }
+
+
             </div>
         </div>
     )
