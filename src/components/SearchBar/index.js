f025e5343fea5a9bf0c868d102a161f8bc785025
Sam <sambisselele@gmail.com>
2022-10-15 17:01:11 +0100

    Feat(Adding search bar)

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { BiSearch } from 'react-icons/bi'
+import { BiSearch } from 'react-icons/bi';
+import "./searchBar.css"
 
 const SearchBar = () => {
     return (
+        <div className="search__bar--container">
+            <form className="search__bar--form">
+                <BiSearch className="search__bar--icon" />
+                <input type="seach" placeholder='Search' className="search__bar--input" />
+            </form>
+        </div>
 
-        <form className="search__bar">
-            <BiSearch />
-            <input type="seach" placeholder='Search' />
-        </form>
 
     )
 }
