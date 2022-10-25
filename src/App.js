0a18f42ba131ea242841a26a8461c8f3b1ef8fa7
Sam <sambisselele@gmail.com>
2022-10-25 13:05:30 +0100

    Refactor(Bad function name for test actions)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignUpPage from "./pages/SignUp";
 import Home from "./pages/Home";
 import { UserContext } from "./Context";
 
-const App = () => {
+function App ()  {
   const { auth, socket } = useContext(UserContext);
   const actualToken = localStorage.getItem("token");
 
